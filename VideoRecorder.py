3f38a5be742f642ad0428a131e5807cce4dd8e9f
Fu-Chun Hsu <fcchsu@student.unimelb.edu.au>
2016-07-22 01:32:09 +1000

    Blue commit video writer

---
+++ VideoRecorder.py
@@ -0,0 +1,52 @@
+#from imutils.piWebStream import WebcamVideoStream
+from piWebStream import WebcamVideoStream
+#from picamera.array import PiRGBArray
+#from picamera import PiCamera
+import argparse
+import imutils
+import time
+import cv2
+from ftplib import FTP
+
+from datetime import date
+import sys
+
+
+#out = cv2.VideoWriter('output.avi',cv2.cv.CV_FOURCC('M','J','P','G'), 20.0, (640,480))
+    
+
+FPS = 15
+PI_ID = 'FF1'   # ID of Device
+TOTAL_SECONDS = 21600  # Change to 30s for test
+
+if __name__ == '__main__':
+    
+    today = date.today()
+    filename=PI_ID + '_' + str(today.month) + '_' + str(today.day) + '_' + str(today.year)
+    fast = WebcamVideoStream(src=0, resolution=(640, 480)).start()
+    time.sleep(5.0)
+
+    print('Start Setting writer')
+    fast.setWriter(filename)
+    if writer is None:
+        print('writer Fail')
+    else:
+        print('writer successfully set')
+    #ftp = ftplib.FTP('1.34.62.109')
+    #ftp.login('bluekidds','aaa71421')
+    #ftp.cwd(')
+    #ftp.retrbinary('RETE '+filename, open(filename,'wb').write)
+    #ftp.storbinary('STOR '+filename, open(filename,'rb'))
+    
+    
+    for _ in xrange(TOTAL_SECONDS):
+        
+        for i in xrange(FPS):
+            frame = fast.read()
+        fast.write(frame)
+	
+
+    cv2.destroyAllWindows()
+    fast.stop()
+    fast.release()
+    out.release()
--- piWebStream.py
+++ piWebStream.py
@@ -3,18 +3,21 @@ from threading import Thread
 import cv2
 
 
-class WebcamVideoStream:
+class WebcamVideoStream(object):
     def __init__(self, resolution=(320, 240), src=0):
         # initialize the video camera stream and read the first frame
         # from the stream
+        self.weight, self.height = resolution
         self.stream = cv2.VideoCapture(src)
-        self.stream.set(3, 640)		# I have found this to be about the highest-
-        self.stream.set(4, 480)
-        (self.grabbed, self.frame) = self.stream.read()
+        self.stream.set(3, self.weight)		# I have found this to be about the highest-
+        self.stream.set(4, self.height)
+        #(self.grabbed, self.frame) = self.stream.read()
+        
 
         # initialize the variable used to indicate if the thread should
         # be stopped
         self.stopped = False
+        self.writter = None
 
     def start(self):
         # start the thread to read frames from the video stream
@@ -34,3 +37,17 @@ class WebcamVideoStream:
 
     def stop(self):
         self.stopped = True
+
+    def setWriter(self, fileName, param=None):
+        #if param is not None:
+        fourcc = cv2.cv.FOURCC(*'mp4v')
+        self.writer = cv2.VideoWriter(fileName, fourcc, 15.0, (int(self.weight),int(self.height)))
+
+    def write(self, frame):
+        if self.writer is not None:
+            self.writer.write(frame)
+
+    def release(self):
+        if self.writer is not None:
+            self.writer.release()
+        self.stream.release()
